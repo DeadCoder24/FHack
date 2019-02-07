9994915c5a0e1541242ad413882529d87d655ff7
topcoder-mc <topcodermc@gmail.com>
2019-02-08 01:24:31 +0330

    work on some bugs in network tools

--- test.py
+++ test.py
@@ -1,38 +1,87 @@
-import scapy.all as scapy
-import argparse
+import logging
+import urllib2 as urllib
+import nmap
 
-def get_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", dest="target",
-                        help="Target IP/IP Range")
-    options = parser.parse_args()
-    return options
-
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1,
-                              verbose=False)[0]
-
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-
-    print("IP\t\t\tMAC Address")
-    print("----------------------------------------------------")
-    for client in clients_list:
-        print(client["ip"] + "\t\t" + client["mac"])
-
-
-options = get_arguments()
-scan_result = scan(options.target)
-
-print
-print
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-ans,unans = arping("192.168.1.82", verbose=0)
-for s,r in ans:
-    print("{} {}".format(r[Ether].src,s[ARP].pdst))
+
+def scanNetwork(network):
+    returnlist = []
+    nm = nmap.PortScanner()
+    a = nm.scan(hosts=network, arguments='-Pn')
+    for k, v in a['scan'].iteritems():
+        if str(v['status']['state']) == 'up':
+            try:
+                returnlist.append([str(v['addresses']['ipv4']), str(v['addresses']['mac'])])
+            except:
+                pass
+    return returnlist
+
+
+def getDefaultInterface(returnNet=False):
+    def to_CIDR_notation(bytes_network, bytes_netmask):
+        network = scapy.utils.ltoa(bytes_network)
+        netmask = 32 - int(round(math.log(0xFFFFFFFF - bytes_netmask, 2)))
+        net = "%s/%s" % (network, netmask)
+        if netmask < 16:
+            return None
+        else:
+            return net
+
+    for network, netmask, _, interface, address in scapy.config.conf.route.routes:
+        if network == 0 or interface == 'lo' or address == '127.0.0.1' or address == '0.0.0.0':
+            continue
+        if netmask <= 0 or netmask == 0xFFFFFFFF:
+            continue
+        net = to_CIDR_notation(network, netmask)
+        if interface != scapy.config.conf.iface:
+            continue
+        if net:
+            if returnNet:
+                return net
+            else:
+                return interface
+
+
+def getGatewayIP():
+    global internetAvail
+    try:
+        internetAvail = True
+        getGateway_p = sr1(IP(dst="google.com", ttl=0) / ICMP() / "XXXXXXXXXXX", verbose=False)
+        return getGateway_p.src
+    except:
+        internetAvail = False
+        print("Cannot get gateway as not connected to internet")
+        return raw_input("Enter gateway address manually: ")
+
+
+def resolveMac(mac):
+    try:
+        url = "http://macvendors.co/api/vendorname/"
+        request = urllib.Request(url + mac, headers={'User-Agent': "API Browser"})
+        response = urllib.urlopen(request)
+        vendor = response.read()
+        vendor = vendor.decode("utf-8")
+        vendor = vendor[:25]
+        return vendor
+    except:
+        return "N/A"
+
+
+#defaultInterface = getDefaultInterface()
+
+print scanNetwork('192.168.1.82')
+
+# defaultInterfaceMac = get_if_hwaddr(defaultInterface)
+# defaultGatewayIP = getGatewayIP()
+#
+# hostsList = scanNetwork(getDefaultInterface(True))
+#
+# if internetAvail:
+#     for i in range(0, len(hostsList)):
+#         mac = hostsList[i][1]
+#         hostsList[i].append(resolveMac(mac))
+#
+# for device in hostsList:
+#     print device
